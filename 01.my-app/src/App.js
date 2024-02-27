6e1bf0bd6829bfff910d5fa54c7be8617802fc99
RYC0208 <ryc0208@naver.com>
2024-02-27 17:24:33 +0900

    2024-02-27 react 첫 시작
    시작입니다

---
+++ 01.my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>react test 하고 있음</h1>
+    </div>
+  );
+}
+
+export default App;
